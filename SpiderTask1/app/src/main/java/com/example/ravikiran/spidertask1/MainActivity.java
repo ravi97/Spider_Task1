fa9c99ddad8f8829db103a51e56f9720a6dec679
ravi <raviramesh.kiran97@gmail.com>
2016-05-28 18:10:17 +0530

    added the project folder and the apk

---
+++ SpiderTask1/app/src/main/java/com/example/ravikiran/spidertask1/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.ravikiran.spidertask1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int number=0;
+    TextView numberDisplay;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        numberDisplay=(TextView)findViewById(R.id.textView);
+    }
+
+    public void increment(View view)
+    {
+      number++;
+      numberDisplay.setText(String.valueOf(number));
+    }
+}
